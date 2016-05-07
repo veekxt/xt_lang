7b95ea6a091b3622b625a4c55286e8cb2c4c003c
XieTao <veekxt@gmail.com>
2016-05-08 04:24:57 +0800

    Tokens 输出提示

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use std::path::Path;
 
 fn main(){
     let tokens = get_tokens_from(Path::new("test/1.xt"));
+    println!("Tokens :");
     for t in tokens {
         t.print();
         print!("\n");
     }
+    println!("Tokens END");
 }
