967a4da075feefc94b083c6293d69c366c2637be
XieTao <veekxt@gmail.com>
2016-05-17 00:43:37 +0800

    文件结构更改

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
 mod lexer;
 mod parser;
 use lexer::*;
-//use parser::*;
+use parser::*;
 use std::path::Path;
-
-pub enum AST {
-    STMT(Vec<AST>),
-    VAR{ iden:Box<AST>, exp: Box<AST> },
-    IF{ exp: Box<AST>, stmt: Box<AST>, else_stmt:Option<Box<AST>>  },
-    WHILE{ exp: Box<AST>, stmt: Box<AST> },
-    FN_CALL{ exp: Box<AST>, arg_list: Box<AST> },
-    ARGS(Vec<AST>),
-    ASSIGN{ left_value:Box<AST>, exp: Box<AST> },
-    INT(isize),
-    STR(String),
-    IDEN(String),
-}
-
-fn build_exp() -> AST {
-    return AST::STR("asd".to_string());
-}
 
 fn main(){
     let tokens = get_tokens_from(Path::new("test/1.xt"));
@@ -29,4 +12,5 @@ fn main(){
         print!("\n");
     }
     println!("Tokens END");
+    test();
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,18 @@
+use lexer::*;
 
+pub enum AST {
+    STMT(Vec<AST>),
+    VAR{ iden:Box<AST>, exp: Box<AST> },
+    IF{ exp: Box<AST>, stmt: Box<AST>, else_stmt:Option<Box<AST>>  },
+    WHILE{ exp: Box<AST>, stmt: Box<AST> },
+    FN_CALL{ exp: Box<AST>, arg_list: Box<AST> },
+    ARGS(Vec<AST>),
+    ASSIGN{ left_value:Box<AST>, exp: Box<AST> },
+    INT(isize),
+    STR(String),
+    IDEN(String),
+}
+
+pub fn exp(tokens: &StatusVec<Token>) -> AST {
+    AST::STR("test".to_string())
+}
