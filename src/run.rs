4acf9e067b5dfd1d2a6948009edabf487a556451
veekxt <veekxt@gmail.com>
2018-03-16 22:21:04 +0800

    debug for iden

--- src/run.rs
+++ src/run.rs
@@ -10,6 +10,49 @@ struct Context {
 }
 
 fn run_exp(ast: &Box<AST>) -> Box<XtValue> {
+    match **ast {
+        AST::STMT(_) => {},
+        AST::VAR { .. } => {},
+        AST::IF { .. } => {},
+        AST::WHILE { .. } => {},
+        AST::BREAK => {},
+        AST::CONTINUE => {},
+        AST::RETURN(_) => {},
+        AST::CALL { .. } => {},
+        AST::ARGS(_) => {},
+        AST::ATTRS(_) => {},
+        AST::FUNCTIONS(_) => {},
+        AST::CLASS { .. } => {},
+        AST::INDEX { .. } => {},
+        AST::ASSIGN { .. } => {},
+        AST::DEVAL { .. } => {},
+        AST::DEF { .. } => {},
+        AST::PLUS { .. } => {},
+        AST::MINUS { .. } => {},
+        AST::NEG(_) => {},
+        AST::MUL { .. } => {},
+        AST::DIV { .. } => {},
+        AST::MOD { .. } => {},
+        AST::LT { .. } => {},
+        AST::LE { .. } => {},
+        AST::GT { .. } => {},
+        AST::GE { .. } => {},
+        AST::EQ { .. } => {},
+        AST::NE { .. } => {},
+        AST::ANDAND { .. } => {},
+        AST::OROR { .. } => {},
+        AST::DOT { .. } => {},
+        AST::NOT(_) => {},
+        AST::FALSE => {},
+        AST::TRUE => {},
+        AST::INT(_) => {},
+        AST::FLOAT(_) => {},
+        AST::STR(_) => {},
+        AST::IDEN(_) => {},
+        AST::NULL => {},
+        AST::END => {},
+        AST::ERR(_, _) => {},
+    }
     new_int(42)
 }
 
@@ -22,15 +65,17 @@ enum StmtInfo {
 
 fn run_var(context: &mut Context, iden: &Box<AST>, exp: &Box<AST>) -> Result<StmtInfo, String> {
     let iden_name = match **iden {
-        AST::STR(ref nstr) => {
+        AST::IDEN(ref nstr) => {
             Some(nstr)
         }
-        _ => { None }
+        _ => {
+            None
+        }
     };
     if let Some(last) = context.symbol_table.last_mut() {
         if let Some(nstr) = iden_name {
             last.insert(nstr.to_string(), *run_exp(exp));
-            println!("Add symbol {}", nstr)
+            println!("Add new symbol: {}", nstr);
         } else {
             // TODO left node is not string
         }
@@ -45,7 +90,7 @@ fn run_stmt(context: &mut Context, ast_list: &Vec<AST>) -> Result<StmtInfo, Stri
     for ast in ast_list.iter() {
         match ast {
             &AST::VAR { ref iden, ref exp } => {
-                run_var(context, &iden, exp);
+                run_var(context, iden, exp);
             }
             &AST::STMT(ref stmt) => {
                 run_stmt(context,stmt);
